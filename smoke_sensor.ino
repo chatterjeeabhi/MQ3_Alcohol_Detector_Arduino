5df0c7bb2cf9f8a02ec0ed72b1fe65992a309d99
Abhishek Chatterjee <141000190+chatterjeeabhi@users.noreply.github.com>
2023-09-18 10:22:43 +0530

    Add files via upload

---
+++ smoke_sensor.ino
@@ -0,0 +1,29 @@
+const int A0_in = 0;
+const int D0 = 8;
+const int led = 13;
+int threshold ;
+int a0_output ;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(D0 , INPUT);
+  pinMode(led , OUTPUT);
+  }
+
+void loop()
+{
+a0_output = analogRead(A0_in);
+threshold = digitalRead(D0);
+Serial.print(" Methane : ");
+Serial.println(a0_output);
+Serial.print(" Threshold : "); 
+Serial.print(threshold); 
+delay(200);
+
+if (threshold == HIGH){
+  digitalWrite( led ,HIGH);
+  }
+else {
+  digitalWrite(led , LOW);
+  }  
+}
